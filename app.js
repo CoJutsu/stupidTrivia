6118b6159457fe85066c32f91254c95271a927bf
PROVideoOnline <45073092+PROVideoOnline@users.noreply.github.com>
2021-02-01 15:23:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,99 @@
+const api_url = "https://opentdb.com/api.php?amount=1&type=multiple";
+const question = document.querySelector("#multi");
+const category = document.querySelector("#category");
+const diff = document.querySelector("#diff");
+const love = document.querySelector(".yeah");
+const hate = document.querySelector(".nope");
+const wrapper = document.querySelector(".wrapper");
+const desire = document.querySelector("#desire");
+let choice = document.querySelector(".choices");
+const fart = new Audio("./sounds/fart.mp3");
+const good = new Audio("./sounds/good.mp3");
+const letsGo = new Audio("./sounds/lets_go.mp3");
+const gone = new Audio("./sounds/gone.mp3");
+let answers = [];
+
+async function getData() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  const iAnswer = data.results[0].incorrect_answers;
+  const cAnswer = [data.results[0].correct_answer];
+  answers = cAnswer.concat(iAnswer);
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+  shuffle(answers);
+
+  question.innerHTML = data.results[0].question;
+  category.innerHTML = data.results[0].category;
+  diff.innerHTML = data.results[0].difficulty;
+  switch (data.results[0].difficulty) {
+    case "easy":
+      diff.style.color = "#00E851";
+      break;
+    case "medium":
+      diff.style.color = "#FFC00F";
+      break;
+    case "hard":
+      diff.style.color = "#FF1110";
+      break;
+  }
+
+  answers.forEach((answer) => {
+    const button = document.createElement("button");
+    const correct = data.results[0].correct_answer;
+    button.innerText = answer;
+    button.classList.add("btn");
+    choice.appendChild(button);
+
+    if (button.innerText === correct) {
+      button.setAttribute("id", "true");
+    }
+
+    let allBtn = document.getElementsByClassName("btn");
+    button.addEventListener("click", function (e) {
+      for (const btn of allBtn) {
+        btn.classList.add("disable");
+      }
+      let selected = e.target;
+      let value = selected.innerText;
+      if (value == correct) {
+        good.play();
+        selected.classList.add("correct");
+      } else {
+        let cBtn = document.getElementById("true");
+        fart.play();
+        selected.classList.add("wrong");
+
+        setTimeout(() => {
+          cBtn.classList.add("correct");
+          good.play();
+        }, 1000);
+      }
+    });
+  });
+}
+
+getData();
+
+love.addEventListener("click", () => {
+  letsGo.play();
+  wrapper.style.display = "none";
+  document.body.style.backgroundColor = "black";
+  document.body.classList.add("center");
+  setTimeout(() => {
+    document.body.classList.add("stage");
+    desire.innerText = "💖";
+  }, 3000);
+  setInterval(() => {
+    location.reload(true);
+  }, 10000);
+});
+
+hate.addEventListener("click", () => {
+  const remove = document.querySelector("#nope");
+  gone.play();
+  setTimeout(() => {
+    remove.style.display = "none";
+  }, 200);
+});
